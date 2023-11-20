fdd99259b2600dd3974fd0f554c0c4922ef1a79a
maicong <cong.md204521@gmail.com>
2023-11-20 08:58:31 +0700

    fix

--- src/screens/Home/Notification/NotificationScreen.js
+++ src/screens/Home/Notification/NotificationScreen.js
@@ -1,55 +1,5 @@
-<<<<<<< HEAD
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import ProfileCard from '../../../components/ProfileCard'
-const NotificationScreen = () => {
-  const ProfileData = [
-    {
-      userId: '1',
-      userName: 'Peter Griffin',
-      avatarImage: 'https://static.wikia.nocookie.net/familyguy/images/a/aa/FamilyGuy_Single_PeterDrink_R7.jpg/revision/latest/scale-to-width-down/1000?cb=20230815202349',
-      isNotFriend: true
-    },
-    {
-      userId: '2',
-      userName: 'Meg Griffin',
-      avatarImage: 'https://static.wikia.nocookie.net/familyguy/images/1/1b/FamilyGuy_Single_MegMakeup_R7.jpg/revision/latest/scale-to-width-down/1000?cb=20200526171840',
-      isNotFriend: true,
-    },    
-    {
-      userId: '3',
-      userName: 'Lois Griffin',
-      avatarImage: 'https://static.wikia.nocookie.net/familyguy/images/7/7c/FamilyGuy_Single_LoisPose_R7.jpg/revision/latest/scale-to-width-down/1000?cb=20200526171843',
-      isNotFriend: true,
-    },    
-    {
-      userId: '4',
-      userName: 'Stewie Griffin',
-      avatarImage: 'https://static.wikia.nocookie.net/familyguy/images/9/90/FamilyGuy_Single_StewieBackpack_R7.jpg/revision/latest/scale-to-width-down/1000?cb=20200526171841',
-      isNotFriend: false
-    },    
-    {
-      userId: '5',
-      userName: 'Chris Griffin',
-      avatarImage: 'https://static.wikia.nocookie.net/familyguy/images/e/ee/FamilyGuy_Single_ChrisText_R7.jpg/revision/latest/scale-to-width-down/1000?cb=20230815202356',
-      isNotFriend: true
-    },
-  ]
-  return (
-    <ScrollView>
-      {
-        ProfileData.map((item)=><ProfileCard 
-        userId={item.userId}
-        avatarImage = {item.avatarImage}
-        userName={item.userName}
-        isNotFriend = {item.isNotFriend}
-        key = {item.userId}
 
-        />)
-      }
-    </ScrollView>
-      
-=======
+
 import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native'
 import React from 'react'
 import NotificationCard from '../../../components/NotificationCard'
@@ -116,7 +66,7 @@ const NotificationScreen = () => {
       />)
       }
     </ScrollView>
->>>>>>> 70d2bedf8b3f0b88fafd2be588a3870c12d3abac
+
   )
 }
 
